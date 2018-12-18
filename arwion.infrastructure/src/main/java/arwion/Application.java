5c922a02af9282bec45ed44677975792718430e4
nielsdelestinne <nielsdelestinne@gmail.com>
2018-12-18 22:15:39 +0100

    Included security module in infrastructure module + create Startup Application

--- arwion.infrastructure/src/main/java/arwion/Application.java
+++ arwion.infrastructure/src/main/java/arwion/Application.java
@@ -1,4 +1,31 @@
 package arwion;
 
-public class Application {
+import arwion.security.domain.model.users.User;
+import arwion.security.domain.model.users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication(scanBasePackages = {
+    "arwion.security"
+})
+public class Application implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        UserRepository userRepository = applicationContext.getBean(UserRepository.class);
+        userRepository.save(new User("niels", "{noop}password", List.of("ADMIN")));
+    }
 }
